18bbd0912f2ed2a1a6b3a76fd65da9b1b6467224
ellieSMASH <eliseaston@gmail.com>
2019-11-01 12:11:51 +0000

    simplify controller logic, reduce number of global variables

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,15 @@
 require 'game'
 
 describe Game do
+  subject(:game) { described_class.new(player1, player2) }
+  let(:player1) {double(:player)}
+  let(:player2) {double(:player)}
 
   context 'attack' do
-    subject(:game) { described_class.new }
-    let(:faduma) {double(:player)}
-    let(:ellie) {double(:player)}
 
     it 'damages the player' do
-      expect(ellie).to receive(:receive_damage)
-      game.attack(ellie)
+      expect(player2).to receive(:receive_damage)
+      game.attack(player2)
     end
 
   end
